af28be747b92b7edd0ca1d9181063def75f3be8b
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2023-01-17 21:18:50 +0100

    Setup Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
-import Fastify from 'fastify';
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
 
-const app = Fastify();
+const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-    return 'Hey'
+app.register(cors)
+app.get('/', async () => {
+    return prisma.habit.findMany();
 })
 
 app.listen({
