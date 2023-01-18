a2617100b0cdca2919bfea85271b3112d3fb374e
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2023-01-19 00:31:18 +0100

    Work on backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import {PrismaClient} from '@prisma/client'
+import {appRoutes} from "./lib/routes";
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-app.get('/', async () => {
-    return prisma.habit.findMany();
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3000
