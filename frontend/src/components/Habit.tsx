35be232c49de3591fd3fa3d34035ff46880fcb90
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2023-01-19 19:27:12 +0100

    Generate squares to fill dates

--- frontend/src/components/Habit.tsx
+++
@@ -1,8 +0,0 @@
-interface  HabitProps {
-    completed: number
-}
-export function Habit (props: HabitProps) {
-    return (
-        <div className="bg-orange-300 w-20 text-blue rounded m-2 text-center">Habit completed {props.completed}</div>
-    )
-}
---
+++ frontend/src/components/HabitDay.tsx
@@ -0,0 +1,7 @@
+export function HabitDay() {
+    return (
+        <div className="grid-rows-7 grid-flow-col gap-3">
+            <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        </div>
+    )
+}
