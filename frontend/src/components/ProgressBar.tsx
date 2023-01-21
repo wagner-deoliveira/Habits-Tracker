48c09c67da05a2f819216664ba3f37c4486e22c4
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2023-01-21 20:32:23 +0100

    Use Progress for Radix UI and work on toggle state

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -1,26 +1,16 @@
+import * as Progress from '@radix-ui/react-progress'
+
 interface ProgressBarProps {
     progress: number;
 }
-export function ProgressBar(props: ProgressBarProps) {
+
+export function ProgressBar ({progress}: ProgressBarProps) {
     return (
-        <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
-            <div
-                role="progressbar"
-                aria-label="Habit's progress completed this day"
-                aria-valuenow={75}
-                className="h-3 rounded-xl bg-violet-600 w-3/4"
-                style={{width: `${props.progress}%`}}
-            >
-            </div>
-        </div>
+        <Progress.Root value={66} className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+            <Progress.Indicator
+                className="h-3 rounded-xl bg-violet-600 max-w-full ease duration-300"
+                style={{width: `${progress}%`}}
+            />
+        </Progress.Root>
     )
 }
-
-//       TODO: Try to use Progress.Indicator radix-ui
-// <Progress.Root className="ProgressRoot" value={66}>
-//     <Progress.Indicator
-//         className="h-3 rounded-xl bg-white-100 w-full mt-4 transition-transform"
-//         style={{transform: `translateX(-${100 - progress}%)`}}
-//     />
-// </Progress.Root>
-// <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -63,7 +63,9 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
 
-        const completedHabits = day?.dayHabits.map((dayHabit: { habit_id: unknown }) => dayHabit.habit_id)
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id;
+        }) ?? []
 
         return {
             possibleHabits,
